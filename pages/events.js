67317821c1e2a45c680d45d98c89da45a4a71ac8
Christopher Casey <chris.casey1234@gmail.com>
2023-05-28 12:09:20 -0700

    builds out pages files stucture

---
+++ pages/events.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Events({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+
+    </main>
+  )
+}
