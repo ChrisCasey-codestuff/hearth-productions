067ca36c03005d05108bd99fc333cf649623ed29
Christopher Casey <chris.casey1234@gmail.com>
2023-05-31 21:01:01 -0700

    begins to build out homepage

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,27 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-black text-white">
+    <div className="flex flex-row justify-center m-5">
+      <h1>Hearth Productions</h1>
+    </div>
+    <div className="flex flex-row justify-center m-5">
+      <h3>Who we are</h3>
+      <p>
+      </p>
+    </div>
+    <div className="flex flex-row justify-center m-5">
+      <p>The Soundsystem</p>
+    </div>
+    <div className="flex flex-row justify-center m-5">
+      <h3>Upcoming events</h3>
+    </div>
+    <div>
 
+    </div>
+    <div className="flex flex-row justify-center m-5">
+      <h3>Photos</h3>
+    </div>
     </main>
   )
 }
